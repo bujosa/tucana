d11229678abb52558c5fcf2dc6c0c1f84bedd00b
bujosa <davidbujosa@gmail.com>
2021-01-05 03:26:46 -0400

    Eliminated roustes and added function on planet service, and add git ignore file

--- main.go
+++ main.go
@@ -1,14 +1,1 @@
 package main
-
-import (
-	"log"
-	"net/http"
-)
-
-func main(){
-	router := NewRouter()
-	server := http.ListenAndServe(":8080", router)
-	log.Fatal(server)
-}
-
-
--- service/planet.service.go
+++ service/planet.service.go
@@ -1,1 +1,17 @@
 package service
+
+type IPlanetService interface {
+	GetAllPlanets() (domain.Planet, error)
+}
+
+type DefaultPlanetService struct {
+	repo domain.IPlanetRespository
+}
+
+func ( s DefaultPlanetService) GetAllPlanets() ([]domain.Planet, error){
+	return s.repo.FindAll()
+}
+
+func NewPlanetService(repository domain.IPlanetRespository) DefaultPlanetService {
+	return DefaultPlanetService(repo: repository)
+}
