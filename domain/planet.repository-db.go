74fbcba3376dd78d84312717de0d96434909a49b
bujosa <davidbujosa@gmail.com>
2021-01-07 04:16:43 -0400

    Added get Session method

--- domain/planet.repository-db.go
+++ domain/planet.repository-db.go
@@ -1,6 +1,20 @@
 package domain
 
+import (
+	"gopkg.in/mgo.v2"
+	// "gopkg.in/mgo.v2/bson"
+)
 type PlanetRepositoryDb struct {
+}
+
+func getSession() *mgo.Session {
+   session, err := mgo.Dial("mongodb://localhost")
+
+   if err != nil {
+	 	panic(err)
+   }
+
+   return session
 }
 
 func (d PlanetRepositoryDb) FindAll() ([]Planet, error) {
